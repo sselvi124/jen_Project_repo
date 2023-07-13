01562525aa83310f8ab9a25ff68a8dbb7b8804c4
sselvi124 <sselvi124@gmail.com>
2023-07-13 14:45:06 +0530

    2nd

--- jen_Project/src/test/java/pack1/Sample1Test.java
+++ jen_Project/src/test/java/pack1/Sample1Test.java
@@ -2,7 +2,7 @@ package pack1;
 
 import org.testng.annotations.Test;
 
-public class Sample1 {
+public class Sample1Test {
 	@Test
 	public void m1() {
 		System.out.println("hello");
--- jen_Project/src/test/java/pack1/Sample2Test.java
+++ jen_Project/src/test/java/pack1/Sample2Test.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Sample2 {
+public class Sample2Test {
 	@Test
 	public  void m2() {
 		WebDriver driver = new ChromeDriver();
